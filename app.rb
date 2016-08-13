dcc9e0b83fbe096d55f9ea92c069c782be0b596c
Dimitar Dimitrov <me@ddimitrov.name>
2016-08-14 02:18:56 +0300

    Permanently store votes in a YAML file

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'yaml/store'
 
 CHOICES = {
   'happy'        => 'Happy',
@@ -20,17 +21,24 @@ end
 post '/cast' do
   @title = 'Благодарим за вашия глас!'
   @vote  = params['vote']
+  @store = YAML::Store.new 'votes.yml'
+  @store.transaction do
+    if @store['votes'] == nil
+      @store['votes'] = {}
+    end
 
-  if votes[@vote]
-    votes[@vote] = votes[@vote] + 1
-  else
-    votes[@vote] = 1
+    if @store['votes'][@vote]
+      @store['votes'][@vote] = @store['votes'][@vote] + 1
+    else
+      @store['votes'][@vote] = 1
+    end
   end
-
   erb :cast
 end
 
 get '/results' do
-  @votes = votes
+  @title = 'Results so far:'
+  @store = YAML::Store.new 'votes.yml'
+  @votes = @store.transaction { @store['votes'] }
   erb :results
 end
